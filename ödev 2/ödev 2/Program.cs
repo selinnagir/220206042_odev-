7f830390477743e8caaf8c66f5e97940e924f48e
selinnagir <152811115+selinnagir@users.noreply.github.com>
2023-12-13 17:55:01 +0300

    Add files via upload

---
+++ ödev 2/ödev 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+public static class StringManipulator
+{
+
+    public static string ReverseString(this string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
+
+
+public static class ArrayOperations
+{
+   
+    public static void SortArray(this int[] array)
+    {
+        Array.Sort(array);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        string text = "message box!";
+        string reversedText = text.ReverseString();
+        Console.WriteLine("Tersine çevrilmiş metin: " + reversedText);
+
+      
+        int[] numbers = { 5, 2, 8, 1, 9 };
+        numbers.SortArray();
+        Console.WriteLine("Sıralanmış dizi: " + string.Join(", ", numbers));
+    }
+}
